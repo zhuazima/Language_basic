556ab286073aff4645ac61eab9fb7b5d0ab99179
zhuazima <1025854946@qq.com>
2023-10-19 19:02:19 +0800

    class

---
+++ python/Object-Oriented Programming/class.py
@@ -0,0 +1,27 @@
+
+#
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+
+st1 = Student('alice ',88)
+
+print(st1.score)
+print(st1.get_grade)
+
+
+
+
+
+
