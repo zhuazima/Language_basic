cc106b32e9dc4650db969388cf1777852f5cb82e
zhuazima <1025854946@qq.com>
2023-11-25 14:17:41 +0800

    增加多文件变成

---
+++ c/Datastructure/structure/structure.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "structure.h"
+
+
+void test_Func(void)
+{
+    printf("i am test_fun\r\n");
+}
---
+++ c/Datastructure/structure/structure.h
@@ -0,0 +1,9 @@
+#ifndef _STRUCTURE_H_
+#define _STRUCTURE_H_
+
+
+void test_Func(void);
+
+
+
+#endif
