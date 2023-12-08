da73b43874d26a7f2a882bc46efdfb22362bcd60
zhuazima <1025854946@qq.com>
2023-12-08 14:56:28 +0800

    添加烧录功能

--- python/J-link_burn.py
+++ python/J-link_burn.py
@@ -1,18 +1,39 @@
 import pylink
 import ctypes, os, sys
 
+hex_file_path = "D:/svn/Peacock/Release/keil/TCPL01_AUTO_SDK_V1.0.11/tools/BusTransceiver_V1.0.0.1/BootloaderV2/bootloader_v2.0.0.5.hex"  # 替换为你的 .hex 文件路径
 chip_name = 'Cortex-M0'
 
-# def connect_to_device(self, chip_name):
-#     try:
-#         index = self.get_device_index(chip_name)
-#         self.open()
-#         self.connect(index)
-#         # 执行其他操作，如读取寄存器、烧录程序等
-#     except errors.JLinkException as e:
-#         print("JLinkException:", e)
+# 烧录
+def flash_hex_file(jlink,file_path):
+    try:
+        # 烧录 .hex 文件
+        jlink.flash_file(file_path,0x00000000)  # 替换为你的 .hex 文件路径
+    except pylink.errors.JLinkException as e:
+        print("JLinkException:", e)
+    except Exception as ex:
+        print("Exception:", ex)
 
-def main():
+
+# 读取内存
+def read_memory(jlink,memory_address,date_len):
+    data_list = jlink.memory_read64(memory_address, date_len)
+
+    if len(data_list) > 0:
+        for i in range(len(data_list)):
+            hex_data = hex(data_list[i])
+            print(hex_data)
+            i = i + 1
+    else:
+        print("No data returned")    
+
+# 断开连接
+def disconnect_jlink(jlink):
+    jlink.close()  
+
+
+
+if __name__ == "__main__":
     try:
         jlink = pylink.JLink()
         jlink.open()  # 连接 J-Link 调试器 ，不需要 S/N 号也可以正常连接 jlink.open("69511641")
@@ -20,38 +41,16 @@ def main():
         jlink.set_speed(1000)  # 设置速度为 1000 kHz
         jlink.connect(chip_name)
 
-
-        # 读取内存
-        data_list = jlink.memory_read64(0x00000040, 0x100)
-
-        if len(data_list) > 0:
-            for i in range(len(data_list)):
-                hex_data = hex(data_list[i])
-                print(hex_data)
-                i = i + 1
-        else:
-            print("No data returned")
-
-
-        # # 设置速度和目标设备
-        # jlink.set_speed(1000)  # 设置速度为 1000 kHz
-        # # jlink.connect(chip_name)  # 替换为你的目标设备
-        # jlink.connect_to_device('Cortex-M0')
-
-        # # 读取设备 ID
-        # device_id = jlink.read_id()
-        # print("Device ID:", device_id)
-
-        # # 这里可以执行其他操作，比如烧录程序、读取/写入寄存器等
-        # # 烧录
- 
-        # # 擦除
-
-        jlink.close()  # 断开连接
     except pylink.errors.JLinkException as e:
         print("JLinkException:", e)
     except Exception as ex:
         print("Exception:", ex)
 
-if __name__ == "__main__":
-    main()
+    #读取
+    read_memory(jlink,0x00000040,0x20)
+
+    #烧录
+    flash_hex_file(jlink,hex_file_path)
+
+    # 断开连接
+    disconnect_jlink(jlink)
