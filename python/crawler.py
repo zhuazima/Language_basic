49370b292d65d914a65949676b7a828247b86dda
zhuazima <1025854946@qq.com>
2023-12-14 10:17:53 +0800

    添加爬虫例子

---
+++ python/crawler.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def download_images(url):
+    # 发送 GET 请求
+    response = requests.get(url)
+
+    # 检查请求状态
+    if response.status_code == 200:
+        # 使用 BeautifulSoup 解析页面内容
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # 找到所有图片标签
+        images = soup.find_all('img')
+
+        # 创建保存图片的文件夹
+        if not os.path.exists('downloaded_images'):
+            os.makedirs('downloaded_images')
+
+        # 下载图片
+        for idx, img in enumerate(images):
+            img_url = img.get('src')
+            img_name = f"image_{idx}.jpg"
+
+            # 下载图片
+            img_data = requests.get(img_url).content
+            with open(f"downloaded_images/{img_name}", 'wb') as f:
+                f.write(img_data)
+            
+            print(f"Downloaded {img_name}")
+
+    else:
+        print("Failed to fetch the page.")
+
+# 要爬取的网页 URL
+url_to_crawl = 'https://www.bing.com/images/search?q=%e5%9b%be%e7%89%87&qpvt=%e5%9b%be%e7%89%87&form=IGRE&first=1'
+
+# 调用函数并传入网址
+download_images(url_to_crawl)
