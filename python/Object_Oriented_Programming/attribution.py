96faed3e37913566f4c8668ce885d54a49960334
zhuazima <1025854946@qq.com>
2023-12-29 17:23:32 +0800

    add attribution file

---
+++ python/Object_Oriented_Programming/attribution.py
@@ -0,0 +1,26 @@
+##新建对象 count 能够递增
+
+# -*- coding: utf-8 -*-
+
+class Student(object):
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
+
+            
