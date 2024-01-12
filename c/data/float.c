9a7cca5dfa2d3c9d544e02127d42cc691cdbce75
zhuazima <1025854946@qq.com>
2024-01-12 13:17:00 +0800

    add file float.c

---
+++ c/data/float.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main( void )
+{
+    /**/
+    printf("%f",1.23e5);
+    
+    
+	return 0;
+}
--- c/data/integer.c
+++ c/data/integer.c
@@ -10,8 +10,7 @@ int main( void )
 	printf("number is dec = %#d, hex = %#x, octal = %#o \r\n",number,number_1,number_2);
 
     /*各种整数占用空间*/
-    printf("short size %d bytes,int size %d bytes,long size %d bytes,long long size %d bytes\r\n",
-	       sizeof(short),sizeof(int),sizeof(long),sizeof(long long));
+    printf("short size %d bytes,int size %d bytes,long size %d bytes,long long size %d bytes,char size %d bytes \r\n",sizeof(short),sizeof(int),sizeof(long),sizeof(long long),sizeof(char));
 
     /*int long 都是占用32位，他们能存储的最大值也是一样的*/
 	int a = 0x7fffffff;
@@ -26,7 +25,10 @@ int main( void )
 	int _ch = 65;
 	char enter = '\n';
 	printf("%c , %c ,%c \r\n", ch,_ch,enter);
+
+	/*转义符*/
 	printf("Gramps sez, \"a \\ is a backslash.\"\n");
+	printf("Hello!\007\n");   // \007 蜂鸣器
 
 	return 0;
 }
