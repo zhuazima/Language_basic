c61d7418d2c46664eee4b05c283c708670cf69c0
zhuazima <1025854946@qq.com>
2024-02-04 22:00:31 +0800

    change afile

--- c/Const/Const.c
+++ c/Const/Const.c
@@ -1,3 +1,11 @@
+/*
+ * @Author: zhuazima 1025854946@qq.com
+ * @Date: 2024-01-24 10:19:14
+ * @LastEditors: zhuazima 1025854946@qq.com
+ * @LastEditTime: 2024-02-04 22:00:08
+ * @FilePath: \Language_basic-main\c\Const\Const.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -19,7 +27,7 @@ int main(void)
 
 	int* const p = &a;
 
-	// int const* const p
+	// int const* const p  
 	const int* const p = &a;
 	// *p = 12;
 	// p = &b;
