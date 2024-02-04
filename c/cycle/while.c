e42db7c6169170570f3cbcfca9d066c0b3e2b05e
zhuazima <1025854946@qq.com>
2024-02-04 21:58:25 +0800

    update gcc

--- c/cycle/while.c
+++ c/cycle/while.c
@@ -1,3 +1,11 @@
+/*
+ * @Author: zhuazima 1025854946@qq.com
+ * @Date: 2024-01-24 10:19:14
+ * @LastEditors: zhuazima 1025854946@qq.com
+ * @LastEditTime: 2024-02-04 21:12:43
+ * @FilePath: \Language_basic-main\c\cycle\while.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include<stdio.h>
 #include<stdlib.h>
 #include <math.h>
