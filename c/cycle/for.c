936ff2df46810f2fd18132c53860d46ba93a8e5e
zhuazima <1025854946@qq.com>
2024-03-18 12:02:13 +0800

    for cycle test

--- c/cycle/for.c
+++ c/cycle/for.c
@@ -1,11 +1,21 @@
+#include  <stdio.h>
 
+int  main(void)
 
-#include <stdio.h>
-
-int main(void)
 {
-    double debt;
-    for (debt = 100.0; debt < 150.0; debt = debt * 1.1)
-    printf("Your debt is now $%.2f.\n", debt);
-    return 0;
+
+const int FIRST_OZ = 46;    // 2013邮资
+
+const int NEXT_OZ = 20;     // 2013邮资
+
+int  ounces,  cost;
+
+printf("  ounces   cost\n");
+
+for  (ounces  =  1,  cost  =  FIRST_OZ;  ounces  <=  16;  ounces++,cost  +=  NEXT_OZ)
+
+printf("%5d    $%4.2f\n",  ounces,  cost  /  100.0);
+
+return  0;
+
 }
