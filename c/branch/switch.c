04e39d809f4eaa0f228529b3c2c854585844bae6
zhuazima <1025854946@qq.com>
2024-03-21 17:36:16 +0800

    add branch files

---
+++ c/branch/switch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 12;
+    switch (i)
+    {
+    case 12:
+        /* code */
+        printf("test");
+        break;
+    
+    default:
+        break;
+    }
+    
+    return 0;
+}
